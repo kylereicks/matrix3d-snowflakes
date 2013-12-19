f2422185ea3dd1e60e9b07435a66ba1288a2f9b9
Kyle Reicks <reicks.kyle@gmail.com>
2013-12-18 18:21:54 -0600

    Cross-browser action

--- js/snowflakes.js
+++ js/snowflakes.js
@@ -1,10 +1,11 @@
 ;(function(window, document, undefined){
+  'use strict';
   var snowflake = function(settings){
     var flake = document.createElement('div'),
     page = document.getElementsByTagName('html')[0],
     pageHeight = +window.getComputedStyle(page).height.substr(0, window.getComputedStyle(page).height.length - 2),
     pageWidth = +window.getComputedStyle(page).width.substr(0, window.getComputedStyle(page).width.length - 2),
-    pageHeightOffset = -pageHeight - 100,
+    pageHeightOffset = -pageHeight,
     movements = {
       tY: utilities.randomRange(5, 20) * 0.1,
       tXdirection: Math.random() < 0.5 ? -1 : 1,
@@ -20,7 +21,8 @@
       rZdirection: Math.random() < 0.5 ? -1 : 1,
       rZfreq: utilities.randomRange(2, 40) * 0.1
     },
-    xOrigin = Math.random() * (pageWidth - 200);
+    initialMatrix = '',
+    xOrigin = utilities.randomRange(0, pageWidth);
     settings.color = settings.color || {};
     settings.color.rRange = settings.color.rRange || [225, 255];
     settings.color.gRange = settings.color.gRange || [225, 255];
@@ -28,8 +30,13 @@
     settings.color.aRange = settings.color.aRange || [0, 1];
     flake.className = 'snowflake snowflake--' + utilities.randomRange(1, settings.symbolArray.length) + ' snowflake--size-' + utilities.randomRange(settings.sizeRange[0], settings.sizeRange[1]);
     flake.style.color = 'rgba(' + utilities.randomRange(settings.color.rRange[0], settings.color.rRange[1]) + ', ' + utilities.randomRange(settings.color.gRange[0], settings.color.gRange[1]) + ', ' + utilities.randomRange(settings.color.bRange[0], settings.color.bRange[1]) + ', ' + (utilities.randomRange(settings.color.aRange[0] * 100, settings.color.aRange[1] * 100) / 100) + ')';
-    flake.style['-webkit-transform'] = 'matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, ' + xOrigin + ', ' + pageHeightOffset + ', 0, 1)';
-    document.body.insertBefore(flake);
+    initialMatrix = 'matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, ' + xOrigin + ', ' + pageHeightOffset + ', 0, 1)';
+    flake.style['-webkit-transform'] = initialMatrix;
+    flake.style['-moz-transform'] = initialMatrix;
+    flake.style['-o-transform'] = initialMatrix;
+    flake.style['-ms-transform'] = initialMatrix;
+    flake.style.transform = initialMatrix;
+    document.body.insertBefore(flake, null);
     matrix3d.translateOrigin(flake, xOrigin, pageHeightOffset);
     fall(flake, pageHeightOffset, movements);
   },
@@ -47,6 +54,7 @@
     translateY = 0,
     frame = 0,
     animate = function(){
+      var frameMatrix = '';
       translateY = move.tY;
       translateX = Math.sin(frame * (deg / move.tXfreq)) * (move.tXdirection * move.tXamp);
       frame = frame + 1;
@@ -66,10 +74,13 @@
       rZ[1] = -Math.sin(move.rZdirection * frame * (deg / move.rZfreq));
       rZ[4] = Math.sin(move.rZdirection * frame * (deg / move.rZfreq));
       rZ[5] = Math.cos(move.rZdirection * frame * (deg / move.rZfreq));
-      el.style['-webkit-transform'] = 'matrix3d(' + matrix3d.x(matrix3d.x(matrix3d.x(matrix3d.x(matrix3d.x(matrix3d.x(initialMatrix, tY), tX), tZ), rX), rY), rZ) + ')';
-      el.style.transform = 'matrix3d(' + matrix3d.x(matrix3d.x(matrix3d.x(matrix3d.x(matrix3d.x(matrix3d.x(initialMatrix, tY), tX), tZ), rX), rY), rZ) + ')';
+      frameMatrix = 'matrix3d(' + matrix3d.x(matrix3d.x(matrix3d.x(matrix3d.x(matrix3d.x(matrix3d.x(initialMatrix, tY), tX), tZ), rX), rY), rZ) + ')';
+      el.style['-webkit-transform'] = frameMatrix;
+      el.style['-moz-transform'] = frameMatrix;
+      el.style['-o-transform'] = frameMatrix;
+      el.style['-ms-transform'] = frameMatrix;
+      el.style.transform = frameMatrix;
     if(el.getBoundingClientRect().top < window.innerHeight){
-        console.log(el.getBoundingClientRect().top - window.innerHeight);
         animateID = window.requestAnimationFrame(animate);
       }else{
         window.cancelAnimationFrame(animateID);
@@ -111,12 +122,20 @@
     },
     translateOrigin: function(element, x, y){
       var el = element,
-      initialOrigin = {};
-      initialOrigin.string = window.getComputedStyle(el)['-webkit-transform-origin'];
+      initialOrigin = {},
+      newOrigin = '',
+      computedStyle = window.getComputedStyle(el);
+      initialOrigin.string = computedStyle['-webkit-transform-origin'] || computedStyle['transform-origin'] || computedStyle.transformOrigin;
       initialOrigin.array = initialOrigin.string.split(' ');
       initialOrigin.x = +initialOrigin.array[0].substr(0, initialOrigin.array[0].length - 2);
       initialOrigin.y = +initialOrigin.array[1].substr(0, initialOrigin.array[1].length - 2);
-      el.style['-webkit-transform-origin'] = (initialOrigin.x + x) + 'px ' + (initialOrigin.y + y) + 'px';
+      newOrigin = (initialOrigin.x + x) + 'px ' + (initialOrigin.y + y) + 'px';
+      el.style['-webkit-transform-origin'] = newOrigin;
+      el.style['-moz-transform-origin'] = newOrigin;
+      el.style['-o-transform-origin'] = newOrigin;
+      el.style['-ms-transform-origin'] = newOrigin;
+      el.style['transform-origin'] = newOrigin;
+      el.style.transformOrigin = newOrigin;
     },
     x: function(a, b){
       return [
@@ -153,6 +172,7 @@
     window.setTimeout(function(){utilities.delay(settings);}, 1000);
   },
   init = function(){
+    this.run = true;
     var snowflakeSettings = window.snowflakeSettings || {},
     style = document.createElement('style'),
     css = '';
@@ -160,16 +180,23 @@
     snowflakeSettings.sizeRange = snowflakeSettings.sizeRange || [1, 20];
     snowflakeSettings.shadow = snowflakeSettings.shadow || '0 0 5px rgba(255, 255, 255, .5)';
     snowflakeSettings.color = snowflakeSettings.color || {};
-    css += '.snowflake{position:fixed;-webkit-transform-style:preserve-3d;tranform:preserve-3d;text-shadow:' + snowflakeSettings.shadow + ';}';
+    css += '.snowflake{position:fixed;-webkit-transform-style:preserve-3d;-moz-transform-style:preserve-3d;-o-transform-style:preserve-3d;-ms-transform-style:preserve-3d;transform-style:preserve-3d;text-shadow:' + snowflakeSettings.shadow + ';}';
     for(var i = 0, symbolArrayLength = snowflakeSettings.symbolArray.length; i < symbolArrayLength; i++){
       css += '.snowflake--' + (i + 1) + ':before{content:"' + snowflakeSettings.symbolArray[i] + '";}';
     }
     for(var j = 0; j + snowflakeSettings.sizeRange[0] <  snowflakeSettings.sizeRange[0] + snowflakeSettings.sizeRange[1]; j++){
       css += '.snowflake--size-' + (j + snowflakeSettings.sizeRange[0]) + '{font-size:' + (j + snowflakeSettings.sizeRange[0]) + 'px;}';
     }
-    style.insertBefore(document.createTextNode(css));
-    document.head.insertBefore(style);
+    style.insertBefore(document.createTextNode(css), null);
+    document.head.insertBefore(style, null);
     snowflakes(snowflakeSettings);
   };
-  window.addEventListener('load', init, false);
+  if(window.addEventListener){
+    window.addEventListener('load', init, false);
+  }else if(window.attachEvent){
+    window.attachEvent('onload', init);
+  }
+  if('complete' === document.readyState && !init.run){
+    init();
+  }
 }(this, document));
